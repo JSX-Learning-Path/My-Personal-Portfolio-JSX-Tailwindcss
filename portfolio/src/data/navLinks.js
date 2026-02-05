a3e975b3d7fa633779a9465f70b3d2bc708a511e
donnie88c <cs_neeko@yahoo.com>
2026-02-05 11:01:01 +0200

    Refactor Button component and update Navbar layout with improved navigation structure

--- portfolio/src/data/navLinks.js
+++ portfolio/src/data/navLinks.js
@@ -1,7 +1,9 @@
-export const navLinks = [
+ const navLinks = [
   { href: "#about", label: "About" },
   { href: "#experience", label: "Experience" },
   { href: "#projects", label: "Projects" },
   { href: "#testimonials", label: "Testimonials" },
   { href: "#contact", label: "Contact" },
 ];
+
+export default navLinks;
--- portfolio/src/layout/Navbar.jsx
+++ portfolio/src/layout/Navbar.jsx
@@ -1,35 +1,39 @@
-import { navLinks } from "../data/navLinks";
-import Button from "../components/Button.jsx";
+import navLink from "../data/navLinks";
+import Button from "../components/Button";
 
-function Header() {
+function Navbar() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-transparent py-5">
-      <nav className=" flex container mx-auto px-6 items-center justify-between">
+    <header className="fixed top-0 left-0  right-0 bg-transparent py-5">
+      <nav className="container mx-auto px-6 flex items-center justify-between">
         <a
           href="#"
           className="text-xl font-bold tracking-tight hover:text-primary"
         >
           DG<span className="text-primary">.</span>
         </a>
-        <ul className="flex items-center gap-4 glass rounded-full px-2 py-1 ">
-          {navLinks.map((link) => (
-            <li
-              key={link.href}
-              className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface"
-            >
-              <a href={link.href}>{link.label}</a>
-            </li>
-          ))}
-        </ul>
-        {/*
-          Call to Actions Button 
-        */}
-    <Button className="ml-4" size="sm">
-      Contact Me
-    </Button>
+        {/*Desktop Navigation */}
+        <div className="flex items-center gap-1">
+          <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
+            {navLink.map((link) => {
+              return (
+                <a
+                  key={link.label}
+                  href={link.href}
+                  className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface"
+                >
+                  {link.label}
+                </a>
+              );
+            })}
+          </div>
+        </div>
+        {/*  CTA*/}
+        <div>
+          <Button size="sm">Contact Me</Button>
+        </div>
       </nav>
     </header>
   );
 }
 
-export default Header;
+export default Navbar;
